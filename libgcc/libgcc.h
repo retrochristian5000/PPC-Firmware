111c905bc2abddfe1a9a9c030fb464996dcb9452
retrochristian5000 <252152985+retrochristian5000@users.noreply.github.com>
2026-08-19 05:31:30 -0400

    libgcc: declare signed remainder helper

--- libgcc/libgcc.h
+++ libgcc/libgcc.h
@@ -16,6 +16,7 @@ typedef int word_type __attribute__ ((mode (__word__)));
 uint64_t __udivmoddi4(uint64_t num, uint64_t den, uint64_t *rem);
 
 int64_t __divdi3(int64_t num, int64_t den);
+int64_t __moddi3(int64_t num, int64_t den);
 uint64_t __udivdi3(uint64_t num, uint64_t den);
 
 uint64_t __umoddi3(uint64_t num, uint64_t den);

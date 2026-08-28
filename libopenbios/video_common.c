a78287d3abdd1047d87a4c627f2e5f9c74a57d6c
retrochristian5000 <252152985+retrochristian5000@users.noreply.github.com>
2026-08-28 05:04:54 -0400

    video: restore copyright attribution

--- libopenbios/video_common.c
+++ libopenbios/video_common.c
@@ -6,7 +6,7 @@
  *
  *     Shared video routines
  *
- *   Copyright (C) 2002, 2003, 2004 Samuel Rydh
+ *   Copyright (C) 2002, 2003, 2004 Samuel Rydh (samuel@ibrium.se)
  *
  *   This program is free software; you can redistribute it and/or
  *   modify it under the terms of the GNU General Public License
